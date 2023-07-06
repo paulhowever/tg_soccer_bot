76c647f37b317cb0cac4341f6b338be3d2eba68e
paul <130692898+paulhowever@users.noreply.github.com>
2023-07-07 00:18:47 +0300

    Create const.py

---
+++ const.py
@@ -0,0 +1,26 @@
+import telebot
+from telebot import types
+my_channel_id = '@zakonbvi'
+TOKEN = '6399120803:AAHBV8jzRR8OF3qZMpbQx476-Sjp3D5PtsQ'
+bot = telebot.TeleBot(TOKEN)
+set_channel = ''
+img1 = open('penalty.png', 'rb')
+
+markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+goal = types.KeyboardButton('УДАРИТЬ')
+markup1.add(goal)
+
+markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+udar1 = types.KeyboardButton('1')
+udar2 = types.KeyboardButton('2')
+udar3 = types.KeyboardButton('3')
+udar4 = types.KeyboardButton('4')
+home = types.KeyboardButton('Домой🥅')
+udar5 = types.KeyboardButton('5')
+markup2.add(udar1, udar2, udar3, udar4, home, udar5)
+
+Media = """Автор бота. Обратная связь!
+discord server: https://discord.gg/eem2NNKa6G
+tg: @paulhowever
+web-site: http://a0836219.xsph.ru/
+"""
